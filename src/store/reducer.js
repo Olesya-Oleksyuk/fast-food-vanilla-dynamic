b41622e793cda827fc10494a3450ba546c969ffd
Olesia <oleoleksiuk@gmail.com>
2023-09-28 22:39:39 +0300

    small fixes in JsDocs

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,13 @@
 import Action from './actions';
 
+
+/**
+ * The initial state of the application. 
+ * @constant {Object}
+ * @property {Array} products - The list of products.
+ * @property {Array} markets - The list of markets.
+ * @property {string} categoryFilter - The category filter. 
+ * */
 const initialState = {
   products: [],
   markets: [],
