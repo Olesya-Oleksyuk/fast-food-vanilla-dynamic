90edb677b1fd31e80e8b0484dba3e4216dbd8c2c
Olesia <oleoleksiuk@gmail.com>
2023-10-04 12:54:23 +0300

    add eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require('path');
+const path = require("path");
+const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = {
-  mode: 'none',
-  entry: './src/app.js',
+  mode: "none",
+  entry: "./src/app.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/',
-    assetModuleFilename: 'i/[name][ext][query]',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/",
+    assetModuleFilename: "i/[name][ext][query]",
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, "dist"),
     },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
+  plugins: [new ESLintPlugin()],
 };
